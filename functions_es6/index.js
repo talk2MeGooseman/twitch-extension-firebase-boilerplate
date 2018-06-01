65e171f4007d8ae9a78102ba288c81f135120ccd
Erik Guzman <me.erik@me.com>
2018-06-01 14:05:41 -0700

    Update some missing init stuff

--- functions_es6/index.js
+++ functions_es6/index.js
@@ -1,9 +1,11 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 const express = require('express');
-const cors = require(cors);
+const cors = require('cors');
 
 const app = express();
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
+let db = admin.firestore();
 
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
